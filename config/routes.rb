8b0b8bf7f912832b320d612fe48a5828b5ec1254
Toni Maki <maki.toni@gmail.com>
2012-05-29 13:58:21 +0300

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 NetworkBusinessGame::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+
+  root :to => 'static_pages#home'
+  
+  match '/signup', to: 'users#new'
 
   match '/help', to: 'static_pages#help' 
-  
   match '/about', to: 'static_pages#about'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +58,7 @@ NetworkBusinessGame::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'static_pages#home'
+
 
   # See how all your routes lay out with "rake routes"
 
