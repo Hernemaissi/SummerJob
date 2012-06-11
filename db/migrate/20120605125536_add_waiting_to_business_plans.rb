dca46bd5636b899ef736ae0800dcba7fd6ee19e3
Toni Maki <maki.toni@gmail.com>
2012-06-11 09:14:41 +0300

    Removed some useless files

--- db/migrate/20120605125536_add_waiting_to_business_plans.rb
+++
@@ -1,6 +0,0 @@
-class AddWaitingToBusinessPlans < ActiveRecord::Migration
-  def change
-    add_column :business_plans, :waiting, :boolean, :default => false
-    add_column :business_plans, :verified, :boolean, :default => false
-  end
-end
