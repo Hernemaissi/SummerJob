75c1772d888e7b8d7dae92bb4600c8811879a4af
Toni Maki <maki.toni@gmail.com>
2012-06-12 17:13:14 +0300

    Getting there

---
+++ app/assets/javascripts/battle.js
@@ -0,0 +1,15 @@
+$(function(){
+if ($('.secret').length) {
+  $(window).keydown(function(e){
+      var key = e.which;
+      if (key === 65) {
+        $(".enemy").slideUp("slow", function(){});
+        $(".desc").html("<p> You have defeated the evil goblin. Congratulations!")
+      }
+        
+      
+
+        
+});
+}
+})
