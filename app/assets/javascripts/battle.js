9fc4d2bdcf83b936c546cac35f441f8267c6bd43
Toni Maki <maki.toni@gmail.com>
2012-06-12 18:00:33 +0300

    Small things

--- app/assets/javascripts/battle.js
+++ app/assets/javascripts/battle.js
@@ -1,15 +1,38 @@
 $(function(){
+  
 if ($('.secret').length) {
   $(window).keydown(function(e){
       var key = e.which;
       if (key === 65) {
-        $(".enemy").slideUp("slow", function(){});
-        $(".desc").html("<p> You have defeated the evil goblin. Congratulations!")
+        attack();
       }
         
       
 
         
 });
+
+var goblin = {
+    hp: 10,
+    attack: 2
+}
+
+var user = {
+   hp: 20,
+   attack: 3
+}
+
+
+function updateStatus() {
+  $(".status").html("<p> Your hp: " + user.hp + "</p><p> Goblin hp: " + goblin.hp);
+}
+
+updateStatus();
+
+function attack() {
+  goblin.hp -= user.attack;
+  updateStatus();
+}
+
 }
 })
