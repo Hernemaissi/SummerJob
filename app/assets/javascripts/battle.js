a7faac9033d9bb7fe0e24433381d6c90d982ed4d
Toni Maki <maki.toni@gmail.com>
2012-06-13 16:10:29 +0300

    Always remember to drink you potions

--- app/assets/javascripts/battle.js
+++ app/assets/javascripts/battle.js
@@ -6,7 +6,9 @@ if ($('.secret').length) {
       if (key === 65 || key === 66) {
         attack(key);
       }
-        
+      if (key === 68) {
+        drink_potion();
+      }
       
 
         
@@ -19,7 +21,8 @@ var goblin = {
 
 var user = {
    hp: 50,
-   attack: 3
+   attack: 3,
+   potions: 3
 }
 
 var rand = 0;
@@ -27,12 +30,13 @@ var user_damage = 0;
 var goblin_damage = 0;
 
 function updateStatus() {
-  $(".status").html("<p> Your hp: " + user.hp + "</p><p> Goblin hp: " + goblin.hp);
+  $(".status").html("<p> Your hp: " + user.hp + "    Potions left: " + user.potions + "</p><p> Goblin hp: " + goblin.hp);
 }
 
-function updateDesc(text) {
+function updateDesc(text, was_attack) {
+  was_attack = typeof was_attack !== 'undefined' ? was_attack : false;
   hit = "";
-  if (user_damage === 0) {
+  if (user_damage === 0 && was_attack) {
     hit = "You miss! "
   }
   $(".desc").html("<p>" + hit + text + "</p>");
@@ -59,6 +63,18 @@ function heavy_attack() {
   }
 }
 
+function drink_potion() {
+  if (user.potions > 0) {
+    healing = Math.floor(Math.random()*5);
+    user.hp += healing;
+    user.potions -= 1;
+    updateDesc("You drink a potion. You are healed " + healing + " hit points");
+    updateStatus();
+  } else {
+    updateDesc("No potions left!");
+  }
+}
+
 updateStatus();
 
 function attack(key) {
@@ -70,7 +86,7 @@ function attack(key) {
   goblin.hp -= user_damage;
   if (goblin.hp > 0) {
     user.hp -= goblin_damage;
-    updateDesc("You strike the goblin for " + user_damage + " damage. The goblin strikes you for " + goblin_damage + " damage!");
+    updateDesc("You strike the goblin for " + user_damage + " damage. The goblin strikes you for " + goblin_damage + " damage!", true);
     updateStatus();
   } else {
     $(".enemy").slideUp("slow", function(){});
