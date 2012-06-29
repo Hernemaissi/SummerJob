6c22a6e58c1a58b7d04bb8722e1776798a83b352
Toni Maki <maki.toni@gmail.com>
2012-06-29 10:27:36 +0300

    Added some fixes to index

--- app/controllers/service_roles_controller.rb
+++ app/controllers/service_roles_controller.rb
@@ -2,6 +2,7 @@ class ServiceRolesController < ApplicationController
 
   def index
     @service_companies = ServiceRole.where("service_type = ?", params[:service_type])
+    @company_title = params[:service_type]
     respond_to do |format|
       format.js
       format.html
