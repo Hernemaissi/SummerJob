2adb185192ed7c7880c93b6af16e5aa5d0f88cb5
Toni Maki <maki.toni@gmail.com>
2012-07-12 12:41:21 +0300

    deleted unneeded migration

--- db/migrate/20120712063840_add_total_customers_to_games.rb
+++
@@ -1,6 +0,0 @@
-class AddTotalCustomersToGames < ActiveRecord::Migration
-  def change
-    add_column :games, :total_customers, :integer, :default => 0
-    add_column :games, :currently_handled_customers, :integer, :default => 0
-  end
-end
