8b595494d3d4ca77331bc57dd2b058fd2e9d5dca
Toni Maki <maki.toni@gmail.com>
2012-08-01 16:00:36 +0300

    Added functionality to changing operator stats, views are still missing

--- app/controllers/operator_roles_controller.rb
+++ app/controllers/operator_roles_controller.rb
@@ -7,4 +7,40 @@ class OperatorRolesController < ApplicationController
       format.html
     end
   end
+
+  def edit
+    @operator_company_role = OperatorRole.find(:id)
+  end
+
+  def update
+    @op = OperatorRole.find(:id)
+    capacity_changed = false
+    type_changed = false
+    if @op.capacity != params[:operator_role][:capacity].to_i
+      capacity_changed = true
+    end
+    if @op.product_type != params[:operator_role][:product_type].to_i
+      type_changed = true
+    end
+    @op.update_attributes(params[:operator_role])
+    if @op.save
+      if @op.company.network
+        if capacity_changed
+          @op.company.network.total_profit -= 10000
+          .score -= 10000
+        end
+        if type_changed
+          @op.company.network.total_profit -= 10000
+          @op.company.network.score -= 10000
+        end
+        if type_changed || capacity_changed
+          @op.company.network.save!
+        end
+      end
+      flash[:success] = "Succesfully updated choices"
+      redirect_to @op.company
+  else
+    render 'edit'
+    end
+  end
 end
