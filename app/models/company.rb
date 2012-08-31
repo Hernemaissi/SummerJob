4e82869448a94f0588d559bd8dc52ffa156725f9
Toni Maki <maki.toni@gmail.com>
2012-08-31 13:55:29 +0300

    Level and type now in strings. Still need to add desc

--- app/models/company.rb
+++ app/models/company.rb
@@ -411,8 +411,24 @@ class Company < ActiveRecord::Base
     self.role.service_level
   end
 
+  def service_level_to_s
+    if self.service_level == 1
+      return "Budget"
+    else
+      return "Luxury"
+    end
+  end
+
   def product_type
     self.role.product_type
+  end
+
+  def product_type_to_s
+    if self.product_type == 1
+      return "Space Hop"
+    else
+      return "Space Station"
+    end
   end
   
   #Checks if two companies are of similar type
