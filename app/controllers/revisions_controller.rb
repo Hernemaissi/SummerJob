d6c5d2fe27d713732d1ea052b68ae9ff397a00e5
Toni Maki <maki.toni@gmail.com>
2012-09-28 11:35:16 +0300

    Canvas is now hidden as default but can be toggled as public

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,6 +1,16 @@
 class RevisionsController < ApplicationController
-
+  before_filter :plan_is_public?
   def show
     @revision = Revision.find(params[:id])
+  end
+
+  private
+
+  def plan_is_public?
+    @revision = Revision.find(params[:id])
+    unless (signed_in? && current_user.isOwner?(@revision.company)) || @revision.company.business_plan.public?
+      flash[:error] = "This company has not set their plan as public"
+      redirect_to @revision.company
+    end
   end
 end
