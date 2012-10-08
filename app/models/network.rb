ddf125a03794cbe72023750f551c85030c148379
Toni Maki <maki.toni@gmail.com>
2012-10-08 13:04:35 +0300

    Another fix

--- app/models/network.rb
+++ app/models/network.rb
@@ -240,12 +240,12 @@ class Network < ActiveRecord::Base
         if self.sales % Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level) == 0
           return self.sales / Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level)
         else
-          return (self.sales / Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level) + 1).to_i
+          return self.sales / Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level) + 1
         end
       end
     else
 
-      return (self.sales / Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level)).ceil
+      return (self.sales.to_f / Company.get_capacity_of_launch(self.operator.product_type, self.operator.service_level)).ceil
     end
   end
 
