57eb303ba2ee8b374db848c33ac162887dbb7373
Toni Maki <maki.toni@gmail.com>
2013-08-15 15:58:12 +0300

    Network reports created from dynamic networks are now visible in the report page

--- app/controllers/company_reports_controller.rb
+++ app/controllers/company_reports_controller.rb
@@ -4,8 +4,13 @@ class CompanyReportsController < ApplicationController
     @company = Company.find(params[:id])
     @company_reports = @company.company_reports.order("year ASC")
     @network_reports = nil
-    if @company.network
-      @network_reports = @company.network.network_reports.order("year ASC")
+    @customer_facing_companies = @company.get_customer_facing_company
+    if @customer_facing_companies != nil
+      @network_reports = []
+       @customer_facing_companies.each do |c|
+         @network_reports.concat(c.role.network_reports.all)
+       end
+       @network_reports.sort_by! { |r| r.year }
     end
   end
 end
