cc53b91ba82c887f54c2006801fc3113538a355d
Toni Maki <maki.toni@gmail.com>
2013-08-22 12:27:22 +0300

    some access control

--- app/controllers/company_reports_controller.rb
+++ app/controllers/company_reports_controller.rb
@@ -1,5 +1,7 @@
 class CompanyReportsController < ApplicationController
 
+  before_filter :company_owner
+
   def show
     @company = Company.find(params[:id])
     @company_reports = @company.company_reports.order("year ASC")
