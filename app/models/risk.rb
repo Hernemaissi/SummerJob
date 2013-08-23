f21c54d7cf38548248ba8c17dc249e37671e1283
Toni Maki <maki.toni@gmail.com>
2013-08-23 12:39:25 +0300

    Removed static penalty from risks as it is not currently used

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -22,7 +22,6 @@ class Risk < ActiveRecord::Base
 
   validates :title, presence: true
   validates :description, :presence => true
-  validates :penalty, :presence => true, :numericality => true
   validates :customer_return, :presence => true, :numericality => { :greater_than_or_equal_to  => 0, :less_than_or_equal_to => 100 }
   validates :possibility,  :presence => true, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
   validates :severity, :presence => true, :numericality => { :greater_than => 0, :less_than_or_equal_to => 10 }
