2da230e8c8d268a4656958de8aaedd6b83885ecd
Toni Maki <maki.toni@gmail.com>
2013-09-10 11:13:05 +0300

    Changed production host to aalto-nsbg for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ NetworkBusinessGame::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => "nsbg-new.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "aalto-nsbg.herokuapp.com" }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
