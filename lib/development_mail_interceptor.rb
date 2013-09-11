4162921f58bb777ca453fe0f33d32721a4c9b2d7
Toni Maki <maki.toni@gmail.com>
2013-09-11 11:56:59 +0300

    Changed mail address to the official one

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,7 +1,7 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "nbgaalto@gmail.com"
+    message.to = "aalto.nsbg@gmail.com"
   end
 end
 
