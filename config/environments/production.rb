c4c08f1fd7e6b933f8b0f8c26e8480e9e04d1513
Toni Maki <maki.toni@gmail.com>
2013-09-30 19:25:18 +0300

    Better fix attempt for contracts.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ NetworkBusinessGame::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += ['contracts.css']
 
   # Generate digests for assets URLs
   config.assets.digest = true
