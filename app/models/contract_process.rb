1c29c574c662976035ba440c7b6c9a7198e5885f
Toni Maki <maki.toni@gmail.com>
2014-06-13 14:40:01 +0300

    Added buttons to handle the contract process straight from the process view

--- app/models/contract_process.rb
+++ app/models/contract_process.rb
@@ -34,9 +34,14 @@ class ContractProcess < ActiveRecord::Base
   end
 
   def next_action_by
-    return nil if first_party.has_contract_with?(second_party)
+    if first_party.has_contract_with?(second_party)
+      c = first_party.get_contract_with(second_party)
+      return nil if !c.under_negotiation
+      return c.negotiation_receiver
+    end
     next_to_act = nil
     next_item = self.rfps.last.bids.last
+    return nil if next_item && next_item.rejected?
     next_item = self.rfps.last if !next_item
     next_to_act = next_item.receiver
     return next_to_act
@@ -45,15 +50,59 @@ class ContractProcess < ActiveRecord::Base
   def next_action
     if first_party.has_contract_with?(second_party)
       contract = first_party.get_contract_with(second_party)
-      return "Contract renegotiation" if contract.under_negotiation
-      return "No action needed"
+      return 0 if contract.under_negotiation
+      return 1
     end
     bid = self.rfps.last.bids.last
     if bid
-      return "Waiting for an answer to bid" if bid.waiting?
-      return "Latest bid rejected"
+      return 2 if bid.waiting?
+      return 3
     end
-    return "Waiting for bids"
+    return 4
+  end
+
+  def status_to_string(status)
+
+    case status
+    when 0
+      return "Contract renegotiation"
+    when 1
+      return "No action needed"
+    when 2
+      return "Waiting for an answer to bid"
+    when 3
+      "Latest bid rejected"
+    when 4
+      return "Waiting for bids"
+    else
+      return "Unkown status"
+    end
+
+  end
+
+  def send_bid?(user)
+    act = next_action
+    if act == 3 || (act == 4 && user == receiver)
+      return true
+    end
+    return false
+  end
+
+  def show_bid?(user)
+    act = next_action
+    if act == 2
+      bid = self.rfps.last.bids.last
+      return user.company == bid.receiver
+    end
+    return false
+  end
+
+  def show_contract?(user)
+    if first_party.has_contract_with?(second_party)
+      contract = first_party.get_contract_with(second_party)
+      return contract.under_negotiation && user.company == contract.negotiation_receiver && (user == receiver || user == initiator)
+    end
+    return false
   end
 
   def other_party(party)
