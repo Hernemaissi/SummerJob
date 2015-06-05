99f18d941edbe17bef11fd212ff246e8ff6112c6
Toni Maki <maki.toni@gmail.com>
2015-06-05 11:40:22 +0300

    Small css change to show pointer icon when mouse over previous or next news. Also made it possible again to create flexible networks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -957,6 +957,22 @@ $(".accordion-toggle").click(function() {
         });
     });
 
+    $(function(){
+        var keys = [38,38,40,40,37,39,37,39,66,65];
+
+        var nextKey = 0;
+        $(window).keydown(function(e){
+            var key = e.which;
+            if (key === keys[nextKey])
+                nextKey++;
+            else
+                nextKey = 0;
+
+            if (nextKey >= keys.length)
+                alert("No cheat codes allowed!");
+        });
+    })
+
     
 })
 
