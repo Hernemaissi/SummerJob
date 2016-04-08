d6855f7d741ef27773f4d5d17cde4a1b91d42a06
Toni M„ki <maki.toni@gmail.com>
2016-04-08 15:56:45 +0300

    Added GPL3 license

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,21 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+
+// This file is part of Network Service Business Game.
+
+//  Network Service Business Game is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  Network Service Business Game is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with Network Service Business Game.  If not, see <http://www.gnu.org/licenses/>
+
+
+// // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
